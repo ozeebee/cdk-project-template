984001c225e907e888543343cd305a77dee93d9e
ozeebee <ozeebee@gmail.com>
2022-03-18 11:44:32 +0100

    bump CDK to v2.17.0

--- src/index.ts
+++ src/index.ts
@@ -35,7 +35,7 @@ const templateDef: Template = {
       name: 'cdkVersion',
       type: 'text',
       message: 'CDK version',
-      initial: '2.10.0'
+      initial: '2.17.0'
     },
     {
       name: 'features',
